fbb4857a54f8f0025599d56a191e022f8af97617
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2025-07-09 21:16:33 +0530

    code

--- Loops/ques6.py
+++ Loops/ques6.py
@@ -1,8 +1,12 @@
 # Print the first N terms of the Fibonacci series.
 
 n = int(input("Enter the number of terms: "))
-a, b = 0, 1
 
-for _ in range(n):
+a = 0
+b = 1
+
+for i in range(n):
     print(a, end=' ')
-    a, b = b, a + b
+    c = a + b
+    a = b
+    b = c
