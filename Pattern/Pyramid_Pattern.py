a88720640c801a6ba4b2cd3f8b212e6f743f21fb
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2025-07-14 20:48:55 +0530

    code

---
+++ Pattern/Pyramid_Pattern.py
@@ -0,0 +1,5 @@
+# Pyramid_Pattern
+
+n = 4
+for i in range(1, n+1):
+    print(" " * (n - i) + "* " * i)
---
+++ Pattern/Right-angled.py
@@ -0,0 +1,16 @@
+# Right-angled Triangle (Increasing Stars)
+
+n = 4
+for i in range(1, n+1):
+    print("* " * i)
+
+
+print("*************************************")
+
+# Inverted Right-angled Triangle
+
+m = 4
+for i in range(m, 0 , -1):
+    print("* " * i)
+
+    
