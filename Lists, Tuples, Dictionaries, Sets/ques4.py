03be6daf742ee628400d4379a5511d04b31c98bc
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2025-08-02 20:57:58 +0530

    code

--- Lists, Tuples, Dictionaries, Sets/ques4.py
+++ Lists, Tuples, Dictionaries, Sets/ques4.py
@@ -4,3 +4,9 @@ def common_elements(list1, list2):
     return list(set(list1) & set(list2))
 
 print(common_elements([1, 3, 4], [4, 6, 8]))
+
+#  Check if two sets are disjoint
+
+set1 = {1, 2}
+set2 = {3, 4}
+print(set1.isdisjoint(set2))  # True
