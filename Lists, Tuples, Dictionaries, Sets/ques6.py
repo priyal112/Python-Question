4e95a67ef6f402424049500539dd6e7e8b9e91fc
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2025-08-04 21:21:29 +0530

    code

---
+++ Lists, Tuples, Dictionaries, Sets/ques6.py
@@ -0,0 +1,12 @@
+# Find the union and intersection of two sets
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+# Union
+union_result = set1.union(set2)
+print("Union:", union_result)
+
+# Intersection
+intersection_result = set1.intersection(set2)
+print("Intersection:", intersection_result)
