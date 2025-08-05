1479ceb86cfb4f5d7108f9b4dc76b8fa45aeb78d
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2025-08-05 21:06:18 +0530

    code

---
+++ Lists, Tuples, Dictionaries, Sets/ques7.py
@@ -0,0 +1,11 @@
+# Convert tuple to string and vice versa
+
+# Convert Tuple to String
+my_tuple = ('H', 'e', 'l', 'l', 'o')
+my_string = ''.join(my_tuple)
+print("Tuple to String:", my_string)
+
+# Convert String to Tuple
+new_string = "World"
+new_tuple = tuple(new_string)
+print("String to Tuple:", new_tuple)
