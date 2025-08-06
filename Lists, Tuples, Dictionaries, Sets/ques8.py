e9d187d8ca41dda0e5bf3d64c26410173a9f24f5
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2025-08-06 21:15:05 +0530

    code

---
+++ Lists, Tuples, Dictionaries, Sets/ques8.py
@@ -0,0 +1,10 @@
+# Check if two dictionaries are equal
+
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 2, 'a': 1}
+
+# Check if they are equal
+if dict1 == dict2:
+    print("The dictionaries are equal.")
+else:
+    print("The dictionaries are not equal.")
